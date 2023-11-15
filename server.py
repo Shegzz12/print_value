757e06eebf5bb349a36029ee7e041ef7f731f7f5
Shegzz12 <150711672+Shegzz12@users.noreply.github.com>
2023-11-15 04:52:37 -0800

    Add files via upload

--- server.py
+++ server.py
@@ -4,11 +4,11 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 # CORS(app)
 # Define an endpoint to fetch the value
-@app.route('/fetch_value', methods=['GET'])
+@app.route('/', methods=['GET'])
 def fetch_value():
     # In this example, we'll return a simple JSON response with a value
     response = jsonify({'value': 42})  # You can replace 42 with the actual value
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True ,port=8080,use_reloader=False)
+    app.run(debug=True)
