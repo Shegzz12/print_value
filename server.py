7e00fd707486657e273e90a561b358ef00527fb8
Shegzz12 <150711672+Shegzz12@users.noreply.github.com>
2023-11-16 01:12:12 -0800

    Add files via upload

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ file_handler.setLevel(WARNING)
 
 # Define an endpoint to fetch the value
 @app.route('/', methods=['GET'])
-def fetch_value():
+def index():
     # In this example, we'll return a simple JSON response with a value
     return render_template("index.html")
 
