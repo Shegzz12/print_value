4c7da76e7c1739a4cc99ca649affd9554459816d
Shegzz12 <150711672+Shegzz12@users.noreply.github.com>
2023-11-15 21:43:13 -0800

    Add files via upload

--- templates/app.js
+++ templates/app.js
@@ -1,26 +1,28 @@
 function updateValue() {
-    console.log("button was clicked!")
+    console.log("button was clicked!");
+
     // Get a value from app.js (replace this with your estimated price)
     let valueFromAppJS = 43;
 
     // Make a GET request to the server's endpoint
-    fetch("http://0.0.0.0:10000/fetch_value")
+    fetch("https://jkl-si93.onrender.com/fetch_value")
         .then(response => response.json())
         .then(data => {
             // Get the value from the server's response
             let valueFromServer = data.value;
 
+            // Choose whether to display the value from the server or app.js
+            let finalValue = valueFromServer; // Change this assignment based on your preference
+
             // Update the content of the HTML element with the ID "output"
             let outputElement = document.getElementById("output");
-            outputElement.textContent = "Value from server: " + valueFromServer;
+            outputElement.textContent = "Final Value: " + finalValue;
         })
         .catch(error => {
             console.error("Error fetching value from server: " + error);
-        });
 
-    // Replace with your estimated price
-    
-    // Update the content of the HTML element with the ID "output"
-    let outputElement = document.getElementById("output");
-    outputElement.textContent = "Value from app.js: " + valueFromAppJS;
+            // If there's an error, display the value from app.js
+            let outputElement = document.getElementById("output");
+            outputElement.textContent = "Value from app.js: " + valueFromAppJS;
+        });
 }
