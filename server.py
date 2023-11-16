a775079af2569e9bf17a42611f18d3040af4f66d
Shegzz12 <150711672+Shegzz12@users.noreply.github.com>
2023-11-15 16:11:44 -0800

    Add files via upload

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, render_template
 from logging import FileHandler, WARNING
-# from flask_cors import CORS
+
 
 app = Flask(__name__, template_folder = 'templates')
 file_handler = FileHandler('errorlog.txt')
 file_handler.setLevel(WARNING)
-# CORS(app)
+
 # Define an endpoint to fetch the value
 @app.route('/', methods=['GET'])
 def fetch_value():
@@ -13,4 +13,4 @@ def fetch_value():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=10000, debug=True)
