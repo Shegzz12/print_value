55ef2d011e418f345ada29d3b465362c3bf56f5c
Shegzz12 <150711672+Shegzz12@users.noreply.github.com>
2023-11-15 23:52:45 -0800

    Add files via upload

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def fetch_value():
     # In this example, we'll return a simple JSON response with a value
     return render_template("index.html")
 
-@app.route('/fetch_value', methods=['GET'])
+@app.route('/', methods=['GET'])
 def fetch_value():
     response = 43
     # In this example, we'll return a simple JSON response with a value
